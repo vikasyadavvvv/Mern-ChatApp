7ccacd2003c56a5973ea5c77de76ad1174b39cc5
vikasyadavvvv <157904214+vikasyadavvvv@users.noreply.github.com>
2024-06-19 14:34:26 +0530

    UPDATED

--- Frontend/src/Component/Sidebar/Conversation.jsx
+++ Frontend/src/Component/Sidebar/Conversation.jsx
@@ -1,33 +1,4 @@
-// Conversations.jsx
-import useGetConversations from "../../hooks/useConversation";
-import { getRandomEmoji } from "../../utils/emoji";
-import Conversation from "./Conversation";
 
-const Conversations = () => {
-	const { loading, conversations } = useGetConversations();
-	console.log("CONVERSATION", conversations);
-
-	return (
-		<div className='py-2 flex flex-col overflow-auto h-full'>
-			{loading ? (
-				<div className='flex justify-center items-center h-full'>
-					<span className='loading loading-spinner mx-auto'></span>
-				</div>
-			) : (
-				conversations.map((conversation, idx) => (
-					<Conversation
-						key={conversation._id}
-						conversation={conversation}
-						emoji={getRandomEmoji()}
-						lastIdx={idx === conversations.length - 1}
-					/>
-				))
-			)}
-		</div>
-	);
-};
-
-export default Conversations;
 
 // Conversation.jsx
 import { useSocketContext } from "../../context/socketContext";
