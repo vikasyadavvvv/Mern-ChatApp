db39b5211a7b4cd3a8a27ff0a232b79ab1d63cbc
vikasyadavvvv <157904214+vikasyadavvvv@users.noreply.github.com>
2024-06-20 03:34:33 +0530

    UPDATED

--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -2,12 +2,12 @@ import Sidebar from "../../Component/Sidebar/Sidebar";
 import MessageContainer from "../../Component/message/MessageContainer";
 
 const Home = () => {
-    return (  
-		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-                <Sidebar />
-                <MessageContainer />
-        </div>
-    );
-}
- 
+  return (
+    <div className="flex flex-col md:flex-row h-full sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+      <Sidebar />
+      <MessageContainer />
+    </div>
+  );
+};
+
 export default Home;
