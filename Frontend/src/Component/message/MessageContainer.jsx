7e9c7593bec43984e1635f7ecad3ae64aab4b601
vikasyadavvvv <157904214+vikasyadavvvv@users.noreply.github.com>
2024-06-19 16:24:37 +0530

    UPDATED

--- Frontend/src/Component/message/MessageContainer.jsx
+++ Frontend/src/Component/message/MessageContainer.jsx
@@ -13,7 +13,7 @@ const MessageContainer = () => {
 	}, [setSelectedConversation]);
 
 	return (
-		<div className='md:min-w-[450px] flex flex-col h-full'>
+		<div className='md:min-w-[450px] flex flex-col h-full overflow-auto'>
 			{!selectedConversation ? (
 				<NoChatSelected />
 			) : (
--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -3,9 +3,13 @@ import MessageContainer from "../../Component/message/MessageContainer";
 
 const Home = () => {
     return (  
-        <div className='flex flex-col sm:flex-row h-screen w-screen rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-            <Sidebar className='w-full sm:w-1/3 md:w-1/4 lg:w-1/5' /> {/* Adjust width based on screen size */}
-            <MessageContainer className='flex-1' /> {/* Fill remaining space */}
+        <div className='flex flex-col sm:flex-row h-screen w-screen overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+            <div className='flex flex-col h-full sm:h-auto sm:w-1/3 md:w-1/4 lg:w-3/5 overflow-auto'>
+                <Sidebar />
+            </div>
+            <div className='flex-1 flex flex-col h-full overflow-auto'>
+                <MessageContainer />
+            </div>
         </div>
     );
 }
