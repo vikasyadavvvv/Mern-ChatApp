f44fb7d6a62753bbe767f9b4b3470697fac9a35e
vikasyadavvvv <157904214+vikasyadavvvv@users.noreply.github.com>
2024-06-20 04:14:42 +0530

    UPDATED

--- Frontend/src/Component/Sidebar/Sidebar.jsx
+++ Frontend/src/Component/Sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
   return (
-    <div className="border-r border-slate-500 p-4 flex flex-col w-full lg:w-1/4 h-full overflow-y-auto lg:overflow-hidden">
+    <div className="border-r border-slate-500 p-4 flex flex-col w-full lg:w-2/4 h-full overflow-y-auto lg:overflow-hidden">
       <SearchInput />
       <div className="divider px-3"></div>
       <div className="flex-grow overflow-y-auto">
