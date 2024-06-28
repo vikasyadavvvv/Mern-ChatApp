04c00978dba6e51832d6b761619b12ac4bedbb69
vikasyadavvvv <157904214+vikasyadavvvv@users.noreply.github.com>
2024-06-28 15:16:47 +0530

    UPDATED

--- Frontend/src/Component/message/MessageContainer.jsx
+++ Frontend/src/Component/message/MessageContainer.jsx
@@ -9,20 +9,20 @@ const MessageContainer = () => {
 	const { selectedConversation, setSelectedConversation } = useConversation();
 
 	useEffect(() => {
-		// cleanup function (unmounts)
+		// Cleanup function (unmounts)
 		return () => setSelectedConversation(null);
 	}, [setSelectedConversation]);
 
 	return (
-		<div className='md:min-w-[450px] flex flex-col'>
+		<div className="md:min-w-[450px] flex flex-col overflow-auto">
 			{!selectedConversation ? (
 				<NoChatSelected />
 			) : (
 				<>
 					{/* Header */}
-					<div className='bg-slate-500 px-4 py-2 mb-2'>
-						<span className='label-text'>To:</span>{" "}
-						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
+					<div className="bg-slate-500 px-4 py-2 mb-2">
+						<span className="label-text">To:</span>{" "}
+						<span className="text-gray-900 font-bold">{selectedConversation.fullName}</span>
 					</div>
 					<Messages />
 					<MessageInput />
@@ -33,16 +33,20 @@ const MessageContainer = () => {
 };
 export default MessageContainer;
 
+
+
 const NoChatSelected = () => {
 	const { authUser } = useAuthContext();
 	return (
-		<div className='flex items-center justify-center w-full h-full'>
-			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-				<p>Welcome 👋 {authUser.fullName} ❄</p>
-				<p>Select a chat to start messaging</p>
-				<TiMessages className='text-3xl md:text-6xl text-center' />
+		<div className="flex items-center justify-center w-full h-full">
+			<div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
+				<p className="text-center">Welcome 👋 {authUser.fullName} ❄</p>
+				<p className="text-center">Select a chat to start messaging</p>
+				<TiMessages className="text-3xl md:text-6xl text-center" />
 			</div>
 		</div>
 	);
 };
+
+
 
